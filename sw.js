5d3a750814b425ba5294c72839c7cd01fdf92027
Zathrock <cryptex666.vdf@gmail.com>
2025-11-25 12:22:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "albaz-simple-v12";
+const CACHE_NAME = "albaz-simple-v13";
 const ASSETS = [
   "./",
   "./index.html",
@@ -26,6 +26,7 @@ self.addEventListener("fetch", (e) => {
   );
 
 });
+
 
 
 
